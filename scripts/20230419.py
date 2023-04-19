3807db07db05ea8e6612de222b6138045639d140
zhouhuafei <1123486116@qq.com>
2023-04-19 18:20:35 +0800

    feat: 🎸 new text

---
+++ scripts/20230419.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import os
+
+filename = os.path.basename(__file__)
+textStr = '大盘暴涨我微涨！大盘微跌我暴跌！王德发！这个辣鸡股市！气的我原地爆炸！'
+voiceArr = ['XiaoxiaoNeural', 'XiaoyiNeural', 'YunjianNeural', 'YunxiaNeural', 'YunxiNeural', 'YunyangNeural']
+
+for v in voiceArr:
+  shell = f'edge-tts --text {textStr} --voice zh-CN-{v} --write-media ./audios/{filename}.{v}.mp3'
+  os.system(shell)
