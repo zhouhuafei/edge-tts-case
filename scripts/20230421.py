d05c0be26460e8ac0fbf22632bb72d0698212791
zhouhuafei <1123486116@qq.com>
2023-04-21 11:52:36 +0800

    feat: 🎸 下雪了，下雪了，股市里来了一群大傻瓜。

---
+++ scripts/20230421.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import os
+
+filename = os.path.basename(__file__)
+textStr = '下雪了，下雪了，股市里来了一群大傻瓜。有人气的头晕，有人张口破骂，有人躺在地上不说话，任由大雪掩埋他。'
+voiceArr = ['XiaoxiaoNeural', 'XiaoyiNeural', 'YunjianNeural', 'YunxiaNeural', 'YunxiNeural', 'YunyangNeural']
+
+for v in voiceArr:
+  shell = f'edge-tts --text {textStr} --voice zh-CN-{v} --write-media ./audios/{filename}.{v}.mp3'
+  os.system(shell)
