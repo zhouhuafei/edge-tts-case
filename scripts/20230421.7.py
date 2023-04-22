ec20e953dc1af395cc96717d5b725f449c6a0ec3
zhouhuafei <1123486116@qq.com>
2023-04-22 16:50:54 +0800

    feat: 🎸 678

--- scripts/20230421.7.py
+++ scripts/20230421.7.py
@@ -3,8 +3,8 @@
 import os
 
 filename = os.path.basename(__file__)
-# 大盘微跌我暴跌
-textStr = '大盘暴涨我微涨！大盘微跌我暴跌！王德发！这个辣鸡股市！气的我原地爆炸！'
+# 股市现状
+textStr = '大盘涨我不涨，我与大盘没来往，大盘跌我也跌，我与大盘要衔接。'
 voiceArr = ['XiaoxiaoNeural']
 
 for v in voiceArr:
