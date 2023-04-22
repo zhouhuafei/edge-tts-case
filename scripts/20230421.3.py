b06a6ab3adc4ee0cba7cbabded0cd9c03ea3c5f3
zhouhuafei2 <1123373059@qq.com>
2023-04-22 13:54:03 +0800

    feat: 🎸 声音更换

--- scripts/20230421.3.py
+++ scripts/20230421.3.py
@@ -5,7 +5,7 @@ import os
 filename = os.path.basename(__file__)
 # 如果鲁迅也炒股
 textStr = '我家门前有两棵树，一棵是绿枣树，另一棵也是绿枣树。'
-voiceArr = ['XiaoxiaoNeural']
+voiceArr = ['YunjianNeural']
 
 for v in voiceArr:
   shell = f'edge-tts --text {textStr} --voice zh-CN-{v} --write-media ./audios/{filename}.{v}.mp3'
