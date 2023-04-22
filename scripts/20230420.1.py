72fa52f6f1b21c34c038ac4a36ea672701817958
zhouhuafei <1123486116@qq.com>
2023-04-22 16:24:16 +0800

    style: 💄 调换顺序

--- scripts/20230420.1.py
+++ scripts/20230420.1.py
@@ -4,7 +4,7 @@ import os
 
 filename = os.path.basename(__file__)
 textStr = '大盘暴涨我微涨！大盘微跌我暴跌！王德发！这个辣鸡股市！气的我原地爆炸！'
-voiceArr = ['XiaoxiaoNeural', 'XiaoyiNeural', 'YunjianNeural', 'YunxiaNeural', 'YunxiNeural', 'YunyangNeural']
+voiceArr = ['XiaoxiaoNeural']
 
 for v in voiceArr:
   shell = f'edge-tts --text {textStr} --voice zh-CN-{v} --write-media ./audios/{filename}.{v}.mp3'
