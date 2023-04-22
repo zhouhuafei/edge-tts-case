f32f622755faec1017497a5e392ae8e08d90637d
zhouhuafei <1123486116@qq.com>
2023-04-22 15:21:20 +0800

    docs: ✏️ 日常工作记录

--- scripts/20230420.1.py
+++ scripts/20230420.1.py
@@ -4,7 +4,7 @@ import os
 
 filename = os.path.basename(__file__)
 textStr = '超级导购是国内一款针对服装鞋帽行业终端系统培训和管理的移动APP。它融合多方面的培训课件，拥有便捷的信息交互功能，并具备改变总部对销售终端的管理、培训、知识分享的功能。'
-voiceArr = ['XiaoxiaoNeural', 'XiaoyiNeural', 'YunjianNeural', 'YunxiaNeural', 'YunxiNeural', 'YunyangNeural']
+voiceArr = ['XiaoxiaoNeural']
 
 for v in voiceArr:
   shell = f'edge-tts --text {textStr} --voice zh-CN-{v} --write-media ./audios/{filename}.{v}.mp3'
--- scripts/20230421.1.py
+++ scripts/20230421.1.py
@@ -3,8 +3,9 @@
 import os
 
 filename = os.path.basename(__file__)
+# 股市里的大傻瓜
 textStr = '下雪了，下雪了，股市里来了一群大傻瓜。有人气的头晕，有人张口破骂，有人躺在地上不说话，任由大雪掩埋他。'
-voiceArr = ['XiaoxiaoNeural', 'XiaoyiNeural', 'YunjianNeural', 'YunxiaNeural', 'YunxiNeural', 'YunyangNeural']
+voiceArr = ['XiaoxiaoNeural']
 
 for v in voiceArr:
   shell = f'edge-tts --text {textStr} --voice zh-CN-{v} --write-media ./audios/{filename}.{v}.mp3'
