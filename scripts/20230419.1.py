210cdaf1d87072d8be39237507365e1beefa6571
zhouhuafei <1123486116@qq.com>
2023-04-22 16:23:15 +0800

    style: 💄 调换顺序

--- scripts/20230419.1.py
+++ scripts/20230419.1.py
@@ -3,8 +3,8 @@
 import os
 
 filename = os.path.basename(__file__)
-textStr = '大盘暴涨我微涨！大盘微跌我暴跌！王德发！这个辣鸡股市！气的我原地爆炸！'
-voiceArr = ['XiaoxiaoNeural', 'XiaoyiNeural', 'YunjianNeural', 'YunxiaNeural', 'YunxiNeural', 'YunyangNeural']
+textStr = '超级导购是国内一款针对服装鞋帽行业终端系统培训和管理的移动APP。它融合多方面的培训课件，拥有便捷的信息交互功能，并具备改变总部对销售终端的管理、培训、知识分享的功能。'
+voiceArr = ['XiaoxiaoNeural']
 
 for v in voiceArr:
   shell = f'edge-tts --text {textStr} --voice zh-CN-{v} --write-media ./audios/{filename}.{v}.mp3'
