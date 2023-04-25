40a156de81fe667d4a3252bdb11273a5ae667f65
zhouhuafei <1123486116@qq.com>
2023-04-25 12:34:29 +0800

    feat: 🎸 70还剩42

---
+++ scripts/20230425.1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import os
+
+filename = os.path.basename(__file__)
+# 70还剩42
+textStr = f'./texts/{filename}.txt'
+voiceArr = ['XiaoxiaoNeural']
+
+for v in voiceArr:
+  shell = f'edge-tts --file {textStr} --voice zh-CN-{v} --write-media ./audios/{filename}.{v}.mp3'
+  os.system(shell)
