242e237879cdbed5bc7ff6dbb99b8b2c7e627e3f
zhouhuafei <1123486116@qq.com>
2023-12-31 14:39:26 +0800

    feat: 🎸 HelloWorld

---
+++ scripts/20231231.1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import os
+
+filename = os.path.basename(__file__)
+# HelloWorld
+textStr = f'./texts/{filename}.txt'
+# voiceArr = ['XiaoxiaoNeural', 'XiaoyiNeural', 'YunjianNeural', 'YunxiaNeural', 'YunxiNeural', 'YunyangNeural']
+# 女声推荐 XiaoxiaoNeural
+# 男声推荐 YunjianNeural
+voiceArr = ['YunjianNeural']
+
+for v in voiceArr:
+  shell = f'edge-tts --file {textStr} --voice zh-CN-{v} --write-media ./audios/{filename}.{v}.mp3'
+  os.system(shell)
